ae63e634f6da13c1b5bd5d1892e8b0b7f4caf30a
Aryan Agrawal <80872992+Codes-of-Aryan@users.noreply.github.com>
2022-11-20 10:24:04 +0800

    fine-tuning of model

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 # Cathay-Hackathon
 Training a Machine Leaning Model to get accurate Carbon Emissions per passenger per flight. <br> 
 
+## Running Instructions
+- Please run the file train_regression.py to train and see the accuracy of the model. 
+
 ## Dataset 
 Data taken from the US bureau of statistics
  <a href='https://www.transtats.bts.gov/Fields.asp?gnoyr_VQ=FIM'> DATABASE </a> <br>
- This produces database such as below: <br>
- $ head T_T100D_SEGMENT_US_CARRIER_ONLY.csv <br>
+ This produces database such as below: <br> <br> 
+ $ head T_T100D_SEGMENT_US_CARRIER_ONLY.csv <br> <br>
 DEPARTURES_PERFORMED,PAYLOAD,SEATS,PASSENGERS,FREIGHT,DISTANCE,RAMP_TO_RAMP,AIR_TIME,AIRLINE_ID,UNIQUE_CARRIER_NAME,ORIGIN_AIRPORT_ID,ORIGIN,DEST_AIRPORT_ID,DEST,AIRCRAFT_GROUP,AIRCRAFT_TYPE,AIRCRAFT_CONFIG,YEAR <br>
 0.00,0.00,0.00,0.00,0.00,2500.00,0.00,0.00,19977,United Air Lines Inc.,14893,SMF,11618,EWR,6,614,1,2022 <br>
 0.00,0.00,0.00,0.00,0.00,472.00,0.00,0.00,19977,United Air Lines Inc.,13871,OMA,11292,DEN,6,614,1,2022 <br>
@@ -18,4 +21,5 @@ This is by far the biggest challenge faced during this project. It would be idea
 past flight record data, however since this data is not publicly available, there had to be some adjustments made:
 (P.S. If you wanna see how much I struggled acquiring a dataset, look at the logs)  
 - Couldn't account for the Aircraft Model (Since the particular database does not disclose that info)
-- However there is an aircraft which helps
+- However there is an aircraft group which helps 
+- The Target variable, i.e, Fuel Burn was estimated using a formula used in addFuel.py (Ideally, using FDR data would make this redundant and allow us to even more accurately predict carbon emissions)
--- train_regression.py
+++ train_regression.py
@@ -1,10 +1,16 @@
 #USE the US_CARRIER_DATASET to train a regression model 
 #that predicts fuel burn (and thus carbon emissions) 
 import numpy as np 
-import pandas as pd 
+import pandas as pd
+import matplotlib.pyplot as plt 
 from sklearn import preprocessing, svm
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+
+from sklearn.feature_selection import RFE
+from sklearn.model_selection import GridSearchCV
+
+
 
 #IMPORTING THE DATASET 
 df = pd.read_csv('US_CARRIER_DATASET.csv')
@@ -13,16 +19,42 @@ df = pd.read_csv('US_CARRIER_DATASET.csv')
 df = df.dropna() #Remove Null Value Records 
 df = df[df.PASSENGERS != 0] #only considering passenger flights
 df = df[df.DISTANCE != 0] # distance must not be 0
+df = df[df.RAMP_TO_RAMP != 0] #Airtime must not be zero 
 
 #PREPROCESSING 
 # PAYLOAD, PASSENGERS, FREIGHT, DISTANCE, RAMP_TO_RAMP, AIR_TIME, AIRCRAFT_GROUP
 # are all the factors that we will use from the database to train the model 
 
-X = np.array(df[['PAYLOAD', 'PASSENGERS', 'FREIGHT', 'DISTANCE', 'RAMP_TO_RAMP', 'AIR_TIME','AIRCRAFT_GROUP']]).reshape(-1, 7)
-y = np.array(df['FUEL_BURN']).reshape(-1, 1)
+X = np.array(df[['PAYLOAD', 'PASSENGERS', 'FREIGHT', 'DISTANCE', 
+                'RAMP_TO_RAMP', 'AIR_TIME','AIRCRAFT_GROUP']]).reshape(-1, 7) #Features 
+y = np.array(df['FUEL_BURN']).reshape(-1, 1) #Target labels 
  
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, stratify=y)
-regr = LinearRegression()
-  
-regr.fit(X_train, y_train)
-print(regr.score(X_test, y_test))
+#Training a Linear Regression Model, STEPS INVOLVED: 
+# 1. Fitting Data to Linear Regression Model 
+# 2. Feature Regularization 
+# 3. Hyperparameter Tuning using Grid Search CV 
+# 4. Scoring the model using Test Data 
+
+#Splitting dataset into training and testing data 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20)
+
+li_reg= LinearRegression()
+li_reg.fit(X_train, y_train) #Fitting the model to Linear Regression
+
+rfe = RFE(li_reg) #Feature Regularization 
+
+hyper_params = [{'n_features_to_select': list(range(1, 14))}]   
+
+#Hyperparameter tuning using Grid Search Cross Validation 
+model_cv = GridSearchCV(estimator = rfe, 
+                        param_grid = hyper_params, 
+                        scoring= 'r2', 
+                        cv = 5, 
+                        verbose = 1,
+                        return_train_score=True)  
+
+model_cv.fit(X_train, y_train)   
+accuracy = model_cv.score(X_test, y_test)
+
+accuracy = round(accuracy*100, 3)
+print('The accuracy of our model is {} %'.format(accuracy))
